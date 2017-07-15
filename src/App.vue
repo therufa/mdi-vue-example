dde41bb0964f4c4d0c8be6db6d9ae0f799ce0ca2
therufa <therufa@gmail.com>
2017-07-15 22:23:34 +0200

    chore(): set up example project with some example icons

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div>
+      mdi vue icons
+    </div>
+    <mdi-coin-icon />
+    <mdi-coins-icon />
+    <mdi-xbox-controller-battery-full-icon />
+  </div>
+</template>
+
+<script>
+import 'mdi-vue/XboxControllerBatteryFullIcon'
+import 'mdi-vue/CoinIcon'
+import 'mdi-vue/CoinsIcon'
+
+export default {
+  name: 'app',
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
